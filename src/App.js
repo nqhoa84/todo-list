d5d14f316dc77d751aaa316a46cfcefcb466e5e1
nqhoa84 <65435864+nqhoa84@users.noreply.github.com>
2023-07-28 15:08:06 +0700

    remove old readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState} from 'react';
+import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(() =>{
+    console.log('this is called once refresh page. ')
+    const storedTodos = JSON.parse(localStorage.getItem('todos'));
+    if(storedTodos.length > 0){
+      setTodos(storedTodos);
+    }
+  }, []); // this is called once refresh page. 
+
+  useEffect(() =>{
+    console.log('this is called once todos changed.')
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]); // this is called once todos changed.
+
+
+  function addTodo(text) {
+    setTodos([...todos, text]);
+  }
+
+  function removeTodo(index) {
+    setTodos(todos.filter((todo, i) => i !== index));
+  }
+
+  function editTodo(index, text) {
+    const newTodos = [...todos];
+    newTodos[index] = text;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo list</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          addTodo(event.target.elements.todo.value);
+          event.target.elements.todo.value = '';
+        }
+
+        }
+      >
+
+        <input type='text' name='todo'></input>
+        <button type='submit'>Add todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input type='text' value={todo} onChange={(event)=> editTodo(index, event.target.value)}></input>
+            
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+
     </div>
   );
 }
